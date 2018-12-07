e14b7b4bf01d78ca6b0eb52e825444d937b51fd9
deepak12 <sdetbatch2018@gmail.com>
2018-12-07 14:59:17 +0530

    Ok

---
+++ src/test/java/OrangeHRM/OrangeHRM/SampleTest.java
@@ -0,0 +1,12 @@
+package OrangeHRM.OrangeHRM;
+
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	@Test
+	public void SampleTest()
+	{
+		System.out.println("Sample test is passed");
+	}
+
+}
